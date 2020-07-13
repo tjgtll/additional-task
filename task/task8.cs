2d88f3f9206eda57d1044a7a8393d9558a3cfd77
Nicholas <53100030+tjgtll@users.noreply.github.com>
2020-07-13 14:44:33 +0300

    Update task8.cs

--- task/task8.cs
+++ task/task8.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
 
+namespace skobki
+{
+    class Program
+    {
+        static void ChekS(string s)
+        {
+            List<char> a = new List<char>() { };
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(') a.Add('(');
+                if (s[i] == '{') a.Add('{');
+
+
+                if (s[i] == '}' && a[a.Count - 1] == '{') a.RemoveAt(a.Count - 1);
+
+
+                if (s[i] == ')' && a[a.Count - 1] == '(') a.RemoveAt(a.Count - 1);
+            }
+            if (a.Count == 0) Console.WriteLine($"{s} - ok");
+            else Console.WriteLine($"{s} - неправильно ");
+        }
+
+        static void Main(string[] args)
+        {
+            // {()()}, {(())}
+            Console.WriteLine("Введите строку со скобками");
+            string s = Console.ReadLine();
+            ChekS(s);
+
+            Console.WriteLine();
+        }
+    }
+}
